7aa6984e1fdf1df3a463faa9c3fb6f0f4de229e1
Vihal Arumasinghe <vihal606@gmail.com>
2026-04-05 13:36:14 +0530

    Added digital prescription and report attaching

--- appointment-service/routes/appointmentRoutes.js
+++ appointment-service/routes/appointmentRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/appointmentController");
+const upload = require("../middleware/upload");
 
-router.post("/", controller.createAppointment);
+router.post("/", upload.single("file"), controller.createAppointment);
 
 router.get("/doctor/:doctorId", controller.getDoctorAppointments);
 
 router.patch("/:id/status", controller.updateStatus);
+router.patch("/:id/prescription", controller.addPrescription);
 
 module.exports = router;
