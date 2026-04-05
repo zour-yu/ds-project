aeeccb4f2848f04e0f8365922bb5c143bce52ba1
Vihal Arumasinghe <vihal606@gmail.com>
2026-04-05 12:32:56 +0530

    Improved the functionality

--- appointment-service/controllers/appointmentController.js
+++ appointment-service/controllers/appointmentController.js
@@ -32,3 +32,47 @@ exports.createAppointment = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+exports.getDoctorAppointments = async (req, res) => {
+  try {
+    const doctorId = req.params.doctorId;
+
+    const appointments = await Appointment.find({ doctorId });
+
+    res.json(appointments);
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+
+exports.updateStatus = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { status } = req.body;
+
+    const appointment = await Appointment.findById(id);
+
+    if (!appointment) {
+      return res.status(404).json({ message: "Not found" });
+    }
+
+    // 🔥 If cancelling → free slot
+    if (status === "rejected") {
+      await axios.patch("http://localhost:5001/api/doctors/free-slot", {
+        doctorId: appointment.doctorId,
+        date: appointment.date,
+        time: appointment.time
+      });
+    }
+
+    appointment.status = status;
+    await appointment.save();
+
+    res.json(appointment);
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
